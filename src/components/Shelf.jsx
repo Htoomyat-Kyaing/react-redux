bae2df84b51de1848e866f3388b56c2d25ddb475
Htoomyat-Kyaing <94776906+Htoomyat-Kyaing@users.noreply.github.com>
2023-12-21 18:15:24 +0630

    added conditions for sold out

--- src/components/Shelf.jsx
+++ src/components/Shelf.jsx
@@ -4,17 +4,18 @@ import { LuCakeSlice, LuIceCream } from "react-icons/lu";
 import { useDispatch } from "react-redux";
 import buyCake from "../redux/cakes/cakeActions";
 import buyIcecream from "../redux/icecreams/icecreamActions";
+import { cn } from "../lib/util";
 
 const Shelf = () => {
   const items = [
     {
-      name: "cakes",
+      name: "Cake",
       count: useSelector((state) => state.cake.cakeCount),
       icon: <LuCakeSlice size={30} />,
       description: "20% discount",
     },
     {
-      name: "icecreams",
+      name: "Ice Cream",
       count: useSelector((state) => state.icecream.icecreamCount),
       icon: <LuIceCream size={30} />,
       description: "Best selling item",
@@ -44,19 +45,41 @@ const Shelf = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center gap-5 my-5 max-w-fit">
+    <div className="flex flex-col items-center justify-center w-full gap-5 px-6 md:px-12">
       {/* Items Display */}
-      <div className="shadow stats">{renderedItems}</div>
+      <div className="flex flex-col w-full gap-5 md:flex-row">
+        {renderedItems}
+      </div>
 
       {/* Buttons Group */}
-      <div className="flex flex-col justify-center gap-5 md:flex-row ">
-        <button className="btn btn-primary" onClick={handleCake}>
+      <div className="flex flex-col justify-center gap-5 md:flex-row">
+        <button
+          className={cn(
+            "btn",
+            items[0].count !== 0 ? "btn-primary" : "btn-disabled"
+          )}
+          onClick={handleCake}
+        >
           Buy cake
         </button>
-        <button className=" btn btn-secondary" onClick={handleIcecream}>
+        <button
+          className={cn(
+            "btn",
+            items[1].count !== 0 ? "btn-secondary" : "btn-disabled"
+          )}
+          onClick={handleIcecream}
+        >
           Buy icecream
         </button>
-        <button className=" btn btn-accent" onClick={handleBoth}>
+        <button
+          className={cn(
+            "btn",
+            items[0].count && items[1].count !== 0
+              ? "btn-accent"
+              : "btn-disabled"
+          )}
+          onClick={handleBoth}
+        >
           Buy Both
         </button>
       </div>
--- src/components/ShelfItem.jsx
+++ src/components/ShelfItem.jsx
@@ -1,10 +1,14 @@
 const ShelfItem = ({ Icon, name, count, description }) => {
   return (
-    <div className="stat">
+    <div className="shadow-md stat">
       <div className="w-8 h-8 stat-figure text-secondary">{Icon}</div>
       <div className="stat-title">{name}</div>
       <div className="stat-value">{count}</div>
-      <div className="stat-desc">{description}</div>
+      {count !== 0 ? (
+        <div className="stat-desc max-w-18 text-wrap">{description}</div>
+      ) : (
+        <div className="stat-desc max-w-18 text-wrap">{`Sorry, all the ${name} has been sold out!`}</div>
+      )}
     </div>
   );
 };
